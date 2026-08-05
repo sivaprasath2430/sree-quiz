650cd67fc0ac9619b03bab09b3a1ef4d29d0e6d9
Siva Prasath <sivasiva30526@gmail.com>
2026-08-05 23:07:47 +0800

    Update QuizMaster AI

--- login.js
+++ login.js
@@ -69,3 +69,21 @@ console.log("User:", data.user);
     }
 
 });
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+togglePassword.addEventListener("click", () => {
+
+    if (password.type === "password") {
+
+        password.type = "text";
+        togglePassword.textContent = "🙈";
+
+    } else {
+
+        password.type = "password";
+        togglePassword.textContent = "👁️";
+
+    }
+
+});
--- resultRoutes.js
+++ resultRoutes.js
@@ -4,6 +4,7 @@
 
 const express = require("express");
 const router = express.Router();
+const db = require("./database");
 
 const resultcontroller = require("./resultcontroller");
 
